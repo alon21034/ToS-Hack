11e36a62bd2df9b2487e9233ee20d78e543a31cb
Vince <lee790909@gmail.com>
2013-12-06 11:58:03 +0800

    temp

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 echo "Start ToS"
 
+echo "Where do you want to go?"
+echo "a/b/c/d/e"
+
 read user_input
+
+adb shell getevent -lp /dev/input/event$eventnum > tmp
+WIDTH=`grep ABS_MT_POSITION_X tmp | grep 'max [0-9]\+' -o | grep '[0-9]\+' -o`
+HEIGHT=`grep ABS_MT_POSITION_Y tmp | grep 'max [0-9]\+' -o | grep '[0-9]\+' -o`
+
+echo "$((0.138))"
 
 case "$user_input" in
   a) 
-    sh click.sh 200 300
-    ;;
-  b)
-    sh click.sh 300 400
+    sh click.sh $((0.138 * $WIDTH)) $((0.37 * $HEIGHT))
     ;;
 esac
