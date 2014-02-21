a92d56b8f14e803fc5cf89579b42ab858fa2b342
alon21034 <lee790909@gmail.com>
2014-02-21 21:06:23 +0800

    reinvent, link to console

--- Parser/src/alon/parser/Main.java
+++ Parser/src/alon/parser/Main.java
@@ -3,6 +3,24 @@ package alon.parser;
 public class Main {
 
 	public static void main(String[] params) {
+
+		boolean isTrainging = false;
+		String filePath = "image_features";
+		
+		for (String arg : params) {
+			// System.out.println(arg);
+			if (arg.startsWith("--training")) {
+				isTrainging = true;
+			} else {
+				filePath = arg;
+			}
+		}
+
 		Parser parser = new Parser();
+		
+		if (isTrainging)
+			parser.train();
+		
+		parser.test(filePath);
 	}
 }
