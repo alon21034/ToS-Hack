c63d152cf4388e42688fdcd76e265f3607cf3d2c
alon21034 <lee790909@gmail.com>
2014-02-26 14:46:47 +0800

    init gui

---
+++ ToS-Hack-GUI/src/alon/gui/Main.java
@@ -0,0 +1,9 @@
+package alon.gui;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+	}
+}
--- compile.sh
+++ compile.sh
@@ -8,3 +8,6 @@ javac -cp Parser/libsvm.jar -d Parser/bin -sourcepath Parser/src Parser/src/alon
 echo 'compile solver...'
 javac -d Solver/bin/ -sourcepath Solver/src Solver/src/stimim/solver/Main.java 
 
+echo 'compile gui...'
+javac -d ToS-Hack-GUI/bin -sourcepath ToS-Hack-GUI/src ToS-Hack-GUI/src/alon/gui/Main.java
+
