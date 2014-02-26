63decf6ef9f7a07fdf39fdb86748328232c11f4c
alon21034 <lee790909@gmail.com>
2014-02-26 16:40:29 +0800

    use WindowBuilder

--- ToS-Hack-GUI/src/alon/gui/Main.java
+++ ToS-Hack-GUI/src/alon/gui/Main.java
@@ -1,9 +1,46 @@
 package alon.gui;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		MainFrame frame = new MainFrame();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(327, 243, 117, 29);
+		contentPane.add(btnNewButton);
 	}
 }
