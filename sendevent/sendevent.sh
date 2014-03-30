c9b412d84d1eba8ac80d56bcae735c35edfe11b7
Stimim <death1048576@gmail.com>
2014-03-30 22:11:16 +0800

    remove redundent events

--- sendevent/sendevent.sh
+++ sendevent/sendevent.sh
@@ -8,6 +8,10 @@ echo "eventnum: " $EVENTNUM
 echo "size: " $SIZE
 
 i=0
+dd if=/sdcard/events.out of=/dev/input/event$EVENTNUM bs=$BLOCKSIZE count=2 skip=$i \
+  > /dev/null 2>&1
+i=2
+
 while [[ $(($i * $BLOCKSIZE)) -lt $SIZE ]]
 do
   dd if=/sdcard/events.out of=/dev/input/event$EVENTNUM bs=$BLOCKSIZE count=$COUNT skip=$i \
